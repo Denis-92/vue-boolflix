abed6b2b975e4bdbdfa859488ad1237744e52a1d
Denis-92 <d.mozenic@gmail.com>
2022-09-30 15:29:59 +0200

    added test axios call

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
   <div id="app">
+    <h1>TEST TEST</h1>
   </div>
 </template>
 
 
 <script>
+import axios from 'axios';
+import { keyAPI } from '@/env.js'
+
 export default {
   name: 'App',
+  created() {
+    this.queryAPI();
+  },
+  methods: {
+    queryAPI() {
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${keyAPI}&query=ritorno+al+futuro`).then(
+        (response) => {
+          console.log('response= ', response);
+        }
+      ).catch(
+        error => {
+          console.log('error= ', error.message);
+        }
+      )
+    }
+  }
 }
 </script>
 
