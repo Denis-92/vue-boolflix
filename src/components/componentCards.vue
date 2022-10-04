20a752e10d2bbf85179319237c777d0ccd9dfd38
Denis-92 <d.mozenic@gmail.com>
2022-10-04 13:35:06 +0200

    added link to componentCards instead of seriesTV

--- src/components/componentCards.vue
+++ src/components/componentCards.vue
@@ -1,13 +1,34 @@
 <template>
     <div>
+        <p> TITOLO: {{ title }} </p>
+        <p> TITOLO ORIGINALE: {{ originalTitle }} </p>
+        <p>
+            LINGUA ORIGINALE:
+            <componentDisplayFlagVue :language="language" />
+        </p>
+        <p> VOTO: {{ vote }} </p>
+        <componentImageVue :image="image" />
     </div>
 </template>
   
   
 <script>
+import componentDisplayFlagVue from '@/components/componentDisplayFlag.vue';
+import componentImageVue from '@/components/componentImage.vue';
 
 export default {
     name: 'componentCards',
+    components: {
+        componentDisplayFlagVue,
+        componentImageVue,
+    },
+    props: {
+        title: String,
+        originalTitle: String,
+        language: String,
+        vote: Number,
+        image: String,
+    },
 }
 </script>
   
